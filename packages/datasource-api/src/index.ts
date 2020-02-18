7b00bb085a8bb44a1dab7f57ae47393b0a859d62
Michelle Miller <chellicmiller@gmail.com>
2020-02-18 16:50:04 -0600

    Use result-api in datasource-api

--- packages/datasource-api/src/index.ts
+++ packages/datasource-api/src/index.ts
@@ -1,30 +1,32 @@
+import { Result } from '@chellimiller/result-api'
+
 /**
  * Function for subscribing to a DataSource.
  * Define this separately so it can be passed into the unsubscribe function on the datasource.
  */
-export type DataSourceSubscriber<T> = (data: T) => void
+export type DataSourceSubscriber<DATA> = (result: Result<DATA, any>) => void
 
 /**
  * A data source that can be subscribed to.
  * Subscribers can recieve updates when the data changes.
  */
-export interface DataSource<T> {
+export interface DataSource<DATA> {
   /**
    * The data at that moment. This should generally not be used.
    */
-  readonly data: T
+  readonly data: DATA
 
   /**
    * Subscribe to the data.
    *
    * @param subscriber Function called when the data changes.
    */
-  subscribe(subscriber: DataSourceSubscriber<T>): this
+  subscribe(subscriber: DataSourceSubscriber<DATA>): this
 
   /**
    * Unsubscribe from data changes.
    *
    * @param subscriber Function that will be removed from the DataSource's subscribers.
    */
-  unsubscribe(subscriber: DataSourceSubscriber<T>): this
+  unsubscribe(subscriber: DataSourceSubscriber<DATA>): this
 }
